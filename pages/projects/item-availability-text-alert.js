7b51d6cfa229d080cc4f154e689e46f85db6442d
mfloyd0 <31052292+mfloyd0@users.noreply.github.com>
2025-04-24 21:49:47 -0500

    Update item-availability-text-alert.js

--- pages/projects/item-availability-text-alert.js
+++ pages/projects/item-availability-text-alert.js
@@ -3,15 +3,11 @@ import {
     Badge,
     Link,
     List,
-    ListItem,
-    SimpleGrid,
-    UnorderedList,
-    Heading,
-    Center
+    ListItem
   } from '@chakra-ui/react'
   import Layout from '../../components/layouts/article'
   import { ExternalLinkIcon } from '@chakra-ui/icons'
-  import { Title, WorkImage, Meta } from '../../components/work'
+  import { Title, Meta } from '../../components/work'
   import P from '../../components/paragraph'
   
   const Work = () => (
